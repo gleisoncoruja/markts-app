ef1e55d920d928c5e95569af9a9e3df1b2dff96b
Gleison Souza <101277114+gleisoncoruja@users.noreply.github.com>
2023-09-26 19:50:28 -0300

    feat: craeted navigation and tabBottom navigation

---
+++ src/navigation/index.tsx
@@ -0,0 +1,49 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { HomeScreen } from "../screens/Home";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Icon from "react-native-vector-icons/Ionicons";
+
+const Tab = createBottomTabNavigator();
+
+export const TabsNavigator = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={({ route }) => {
+        const screenInfo = {
+          Home: {
+            iconName: "home",
+            title: "Home",
+          },
+          PendingList: {
+            iconName: "list",
+            title: "Tarefas",
+          },
+        }[route.name];
+        return {
+          tabBarIcon: ({ color, size }) => {
+            const iconSize = 24;
+
+            return (
+              <Ionicons
+                name={screenInfo?.iconName || "home"}
+                size={iconSize}
+                color={color}
+              />
+            );
+          },
+
+          tabBarActiveTintColor: "#1565c0",
+          tabBarInactiveTintColor: "#757575",
+          tabBarStyle: { height: 60, backgroundColor: "#f5f5f5" },
+          tabBarLabelStyle: { fontSize: 12, marginBottom: 4 },
+          tabBarLabel: screenInfo?.title,
+          title: screenInfo?.title,
+        };
+      }}
+    >
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="PendingList" component={HomeScreen} />
+    </Tab.Navigator>
+  );
+};
